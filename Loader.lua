a4dc64ff777cf1cf627238c31d907f978ef21825
evelynnscripts <evelynnscripts@gmail.com>
2025-08-26 16:35:55 +0200

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,52 @@
+local players = game:GetService("Players")
+local player = players.LocalPlayer
+local run = game:GetService("RunService")
+local place = game.PlaceId
+
+local games = {
+
+    [2753915549]      = { name = "bloxfruitssea1", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits.lua" },
+    [4442272183]      = { name = "bloxfruitssea2", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits-sea2.lua" },
+    [7449423635]      = { name = "bloxfruitssea3", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits-sea3.lua" },
+
+    [12355337193]     = { name = "mvsd", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/mvsd-loader.lua" },
+    [126884695634066] = { name = "growagarden", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/grow-a-garden.lua" },
+    [83942919686609]  = { name = "buildascamempire", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/build-a-scam-empire.lua" },
+    [89343390950953]  = { name = "mysingingbrainrot", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/my-singing-brainrot.lua" },
+    [9865958871]      = { name = "bigpaintball2", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/big-paintball-2.lua" },
+    [1240123653]      = { name = "zombieattack", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/zombie-attack.lua" },
+    [17625359962]     = { name = "rivals", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/rivals.lua" },
+    [537413528]       = { name = "buildaboatfortreasure", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/babft.lua" }
+}
+
+
+--// Loader Helpers \\--
+local function supported(id)
+    return games[id] ~= nil
+end
+local function data(id)
+    return games[id]
+end
+local function script(url)
+    local ok, _ = pcall(function()
+        loadstring(game:HttpGet(url, true))()
+    end)
+    if not ok then
+        player:Kick("Script Failed To Load!\nPlease Report This!")
+    end
+end
+local function not_supported()
+    pcall(function()
+        player:Kick("Game is not supported!")
+    end)
+end
+local function loader()
+    if supported(place) then
+        local g = data(place)
+        script(g.url)
+    else
+        not_supported()
+    end
+end
+run.Heartbeat:Wait()
+loader()
