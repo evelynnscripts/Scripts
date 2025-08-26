aaf9d06027c05221b1cc53646efe6e14cbf1a462
evelynnscripts <evelynnscripts@gmail.com>
2025-08-26 23:52:04 +0200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,51 +1,26 @@
-local players = game:GetService("Players")
-local player  = players.LocalPlayer
-local run     = game:GetService("RunService")
-local place   = tostring(game.PlaceId)
-local games = {
-    ["2753915549"]      = { name = "bloxfruits_sea1", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits.lua" },
-    ["4442272183"]      = { name = "bloxfruits_sea2", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits-sea2.lua" },
-    ["7449423635"]      = { name = "bloxfruits_sea3", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/blox-fruits-sea3.lua" },
+local scripts = {
+    [2753915549]      = "blox-fruits.lua", -- first sea
+    [4442272183]      = "blox-fruits.lua", -- second sea
+    [7449423635]      = "blox-fruits.lua", -- third sea
 
-    ["12355337193"]     = { name = "mvsd", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/mvsd-loader.lua" },
-    ["13771457545"]     = { name = "mvsd_pro", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/mvsd-loader.lua" },
+    [12355337193]     = "mvsd-loader.lua", -- normal lobby
+    [13771457545]     = "mvsd-loader.lua", -- pro lobby
 
-    ["1240123653"]      = { name = "zombieattack", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/zombie-attack.lua" },
-    ["1632210982"]      = { name = "zombieattack_pro", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/zombie-attack.lua" },
+    [1240123653]      = "zombie-attack.lua", -- easy mode
+    [1632210982]      = "zombie-attack.lua", -- hard mode
 
-    ["126884695634066"] = { name = "growagarden", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/grow-a-garden.lua" },
-    ["83942919686609"]  = { name = "buildascamempire", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/build-a-scam-empire.lua" },
-    ["89343390950953"]  = { name = "mysingingbrainrot", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/my-singing-brainrot.lua" },
-    ["9865958871"]      = { name = "bigpaintball2", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/big-paintball-2.lua" },
-    ["17625359962"]     = { name = "rivals", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/rivals.lua" },
-    ["537413528"]       = { name = "buildaboatfortreasure", url = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/babft.lua" }
+    [126884695634066] = "grow-a-garden.lua",
+    [83942919686609]  = "build-a-scam-empire.lua",
+    [89343390950953]  = "my-singing-brainrot.lua",
+    [9865958871]      = "big-paintball-2.lua",
+    [17625359962]     = "rivals.lua",
+    [537413528]       = "babft.lua"
 }
-local function supported(id)
-    return games[id] ~= nil
-end
-local function data(id)
-    return games[id]
-end
-local function script(url)
-    local ok, err = pcall(function()
-        loadstring(game:HttpGet(url, true))()
-    end)
-    if not ok then
-        player:Kick("Script Failed To Load!\nPlease Report This!")
-    end
-end
-local function not_supported()
-    pcall(function()
-        player:Kick("Game is not supported!")
-    end)
-end
-local function loader()
-    if supported(place) then
-        local g = data(place)
-        script(g.url)
-    else
-        not_supported()
-    end
-end
-run.Heartbeat:Wait()
-loader()
+
+local placeId = game.PlaceId
+local v = "https://raw.githubusercontent.com/evelynnscripts/Scripts/refs/heads/main/Loader/"
+local function found(id) return scripts[id] ~= nil end
+local function load(id) if not found(id) then game.Players.LocalPlayer:Kick("Game is not supported!") return end
+local url = v .. scripts[id]
+local ok, _ = pcall(function() loadstring(game:HttpGet(url, true))() end) if not ok then game.Players.LocalPlayer:Kick("Script Failed To Load!") end end
+load(placeId)
